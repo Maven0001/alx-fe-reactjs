70e5d9a23246e15b212285973d39268958ece830
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-04 23:30:05 +0100

    Design a counter

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <div>
+      <footer style={{ backgroundColor: "gray" }}>
+        <p
+          style={{
+            color: "white",
+            fontSize: "1em",
+          }}
+        >
+          © 2023 City Lovers
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <div>
+      <main style={{ margin: "auto", backgroundColor: "white" }}>
+        <p style={{ fontSize: "1em", color: "black" }}>
+          I love to visit New York, Paris, and Tokyo.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default MainContent;
