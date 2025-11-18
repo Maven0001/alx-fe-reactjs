b7c389dc714baff321323f942e1158560370a176
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-18 19:25:23 +0100

    integrating the github API and creating a Basic Search feature

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -8,6 +8,11 @@ function UserSearch() {
     setSearch(event.target.value);
   }
 
+  function handleSearchResult(e) {
+    setSearch(e.target.value);
+    e.preventDefault();
+  }
+
   const searchResults = async () => {
     await axios
       .get("https://api.github.com/users/{username}")
@@ -22,15 +27,23 @@ function UserSearch() {
 
   return (
     <>
-      <form action="">
-        <input
-          value={search}
-          type="text"
-          placeholder="Search for a username"
-          onChange={handleUserSearch}
-          className="border-2 border-black m-10 p-2 w-400 rounded-lg placeholder: text-gray-400 text-base  value: text-black"
-        />
-      </form>
+      <div className=" mt-20 ml-20 mb-5 flex gap-2 border-3 border-black">
+        <form action="">
+          <input
+            value={search}
+            type="text"
+            placeholder="Search for a username"
+            onChange={handleUserSearch}
+            className="m-auto border-2 border-black m-10 p-2 w-400 rounded-lg placeholder: text-gray-400 text-base  value: text-black"
+          />
+        </form>
+        <button
+          onSubmit={handleSearchResult}
+          className="border-2 p-2 w-400 rounded-lg bg-blue-800 text-white mt-0"
+        >
+          Search
+        </button>
+      </div>
       <p>Search:{search}</p>
     </>
   );
