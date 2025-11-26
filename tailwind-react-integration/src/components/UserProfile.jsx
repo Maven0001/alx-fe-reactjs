5083a30b3009bfc7e3fd8b65bedd8fbad6cde49d
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-26 11:59:57 +0100

    tailwind integration in react

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 hover:shadow-xl">
+    <div className="bg-gray-100 p-8 max-w-xs mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8  hover:shadow-xl">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 md:w-36 hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">
         John Doe
