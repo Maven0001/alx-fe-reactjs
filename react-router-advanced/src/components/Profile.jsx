f23433f7d668f794ec698f7e9809d3663e95e9cc
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-12-14 23:06:21 +0100

    Routing

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,10 +1,14 @@
-import { Link, Outlet } from "react-router-dom";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
 
 function Profile() {
   return (
     <div>
       <h2>Profile</h2>
-
+      <Routes>
+        <Route path="/ProfileDetails" element={<ProfileDetails />} />
+        <Route path="/ProfilSettings" element={<ProfileSettings />} />
+      </Routes>
       <nav>
         <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
       </nav>
