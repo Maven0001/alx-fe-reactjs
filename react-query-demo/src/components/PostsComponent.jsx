b5fc65db97bca4d2a0ca6a9a3e762487664b58c0
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-12-14 22:37:27 +0100

    update querry

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 const fetchPosts = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   if (!response.ok) {
-    throw new Error("Network response was not ok");
+    throw new Error("Error fetching posts");
   }
   return response.json();
 };
@@ -17,8 +17,10 @@ function PostsComponent() {
     refetch,
     isFetching,
   } = useQuery("posts", fetchPosts, {
-    staleTime: 1000 * 60, // 1 minute cache freshness
-    cacheTime: 1000 * 60 * 5, // 5 minutes in cache
+    staleTime: 1000 * 60,
+    cacheTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
   });
 
   if (isLoading) {
@@ -31,17 +33,13 @@ function PostsComponent() {
 
   return (
     <div>
-      <button onClick={refetch} style={{ marginBottom: "10px" }}>
-        Refetch Posts
-      </button>
+      <button onClick={refetch}>Refetch Posts</button>
 
       {isFetching && <p>Updating data...</p>}
 
       <ul>
         {posts.slice(0, 10).map((post) => (
-          <li key={post.id}>
-            <strong>{post.title}</strong>
-          </li>
+          <li key={post.id}>{post.title}</li>
         ))}
       </ul>
     </div>
