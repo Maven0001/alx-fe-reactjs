ded354f301fe932617671a2f47639ac9b2f8a0c1
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-12-15 12:01:59 +0100

    Update file

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,68 +1,53 @@
 import { useState } from "react";
 
-const RegistrationForm = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+function RegistrationForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const { username, email, password } = formData;
-
     if (!username || !email || !password) {
-      setError("All fields are required");
+      alert("All fields are required");
       return;
     }
 
-    setError("");
-    console.log("User Registered:", formData);
+    console.log({ username, email, password });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Register (Controlled Form)</h2>
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          value={username}          {/* REQUIRED */}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      <div>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}             {/* REQUIRED */}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
 
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        value={formData.username}
-        onChange={handleChange}
-      />
-
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
-      />
-
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-      />
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}          {/* REQUIRED */}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
 
       <button type="submit">Register</button>
     </form>
   );
-};
+}
 
 export default RegistrationForm;
